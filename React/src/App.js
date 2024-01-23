fdbbf5b0914ed4ece04e172a038f0a1e177bb747
parveen kumar <parveen297m210@gmail.com>
2024-01-23 17:24:07 +0530

    update assignment

--- React/src/App.js
+++ React/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to React!
+          Welcome to Aesthisia, You are almost there!
         </p>
         <a
           className="App-link"
